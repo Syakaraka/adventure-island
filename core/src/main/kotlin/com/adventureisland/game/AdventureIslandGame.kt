0a25890020048e49bfaae660c7645dac67ae6bce
Syakaraka <Syakaraka@users.noreply.github.com>
2026-03-20 17:32:26 +0800

    Simplify to basic blue screen to test if app launches

--- core/src/main/kotlin/com/adventureisland/game/AdventureIslandGame.kt
+++ core/src/main/kotlin/com/adventureisland/game/AdventureIslandGame.kt
@@ -2,6 +2,8 @@ package com.adventureisland.game
 
 import com.badlogic.gdx.Game
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Screen
+import com.badlogic.gdx.graphics.GL20
 import com.adventureisland.game.screens.GameScreen
 
 /**
@@ -13,9 +15,26 @@ class AdventureIslandGame : Game() {
     override fun create() {
         try {
             Gdx.app.log("AdventureIslandGame", "create() called")
-            // 启动游戏主界面
-            setScreen(GameScreen(this))
-            Gdx.app.log("AdventureIslandGame", "GameScreen set")
+            
+            // 先设置一个简单的清屏屏幕，测试基本功能
+            setScreen(object : Screen {
+                override fun show() {
+                    Gdx.app.log("TestScreen", "show() called")
+                }
+                
+                override fun render(delta: Float) {
+                    Gdx.gl.glClearColor(0.4f, 0.6f, 0.9f, 1f)
+                    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+                }
+                
+                override fun resize(w: Int, h: Int) {}
+                override fun pause() {}
+                override fun resume() {}
+                override fun hide() {}
+                override fun dispose() {}
+            })
+            
+            Gdx.app.log("AdventureIslandGame", "Test screen set")
         } catch (e: Exception) {
             Gdx.app.error("AdventureIslandGame", "Error in create()", e)
             throw e
